f445243cd6c0dae44f9d9ccc9ad5ff88d2da71c7
chuang hao wei <spirit0919hd@gmail.com>
2024-11-03 22:37:43 +0800

    feat: Home Page UI Add HeaderBox, TotalbalanceBox: DoughnutCahrt, AnimatedCounter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalbalanceBox from "@/components/ui/TotalbalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Howard" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guset"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+
+        <TotalbalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
